a24d4e5fd923d6ad7e582e3c8206eff6b1f5a207
Maksim Khrabryi <Max.xaker41@mail.ru>
2022-12-29 13:30:00 +0400

    launch design

--- AIApp/Modules/Onboarding/OnboardingViewController.swift
+++ AIApp/Modules/Onboarding/OnboardingViewController.swift
@@ -1,29 +1,19 @@
-//
-//  OnboardingViewController.swift
-//  AIApp
-//
-//  Created by Max Xaker on 29.12.2022.
-//
-
 import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    var mainView: OnboardingView {
+        return view as! OnboardingView
+    }
+    
+    override func loadView() {
+        view = OnboardingView()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
