8264c19d673846116a4efa84d287e7e26869b788
Maksim Khrabryi <Max.xaker41@mail.ru>
2022-12-29 13:00:04 +0400

    configure porject

---
+++ AIApp/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  AIApp
+//
+//  Created by Max Xaker on 29.12.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
