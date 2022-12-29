8eda6ae5094de2c191c8a2143af6056f31d255ef
Maksim Khrabryi <Max.xaker41@mail.ru>
2022-12-29 13:24:51 +0400

    add launch images

---
+++ AIApp/Modules/Onboarding/View/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  AIApp
+//
+//  Created by Max Xaker on 29.12.2022.
+//
+
+import UIKit
+
+class OnboardingView: RootView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
